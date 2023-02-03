0adf8288be13649b5c7c4b0a8220dd1636110e05
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-02-04 00:33:25 +0100

    Ignore warnings

--- src/elements/header.tsx
+++ src/elements/header.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import React from 'react';
 import { FaDiscord, FaGithub } from 'react-icons/fa';
