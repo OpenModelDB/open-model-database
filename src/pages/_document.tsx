ecd0cf3731b9d79887bc5396e77d0428f21dab11
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-02-05 17:10:18 -0500

    Set up NextUI (#5)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,14 +1,15 @@
+import { CssBaseline } from '@nextui-org/react';
 import { Head, Html, Main, NextScript } from 'next/document';
 import Script from 'next/script';
 
 // This little script will read the last theme from localStorage and assign it to the HTML element.
-// This ensure that the website is always displayed with the correct them.
+// This ensure that the website is always displayed with the correct theme.
 const themeInit = `(()=>{var t=localStorage.getItem('theme');if(t)document.documentElement.dataset.theme=t})()`;
 
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>{CssBaseline.flush()}</Head>
             <body>
                 <Main />
                 <NextScript />
