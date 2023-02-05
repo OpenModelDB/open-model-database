476b04ccdcde7bf111fe44cb36a53916dbba8f06
Joey Ballentine <joeyjballentine@gmail.com>
2023-02-05 16:42:32 -0500

    Set up NextUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '../styles/globals.scss';
+import { NextUIProvider } from '@nextui-org/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <NextUIProvider>
+            <Component {...pageProps} />
+        </NextUIProvider>
+    );
 }
