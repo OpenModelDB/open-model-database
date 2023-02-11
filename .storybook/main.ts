522eb692524b6ccdb26677f88d8ac70339b9e1a5
Michael Schmidt <mitchi5000.ms@googlemail.com>
2023-02-11 23:13:25 +0100

    Added Storybook 7

---
+++ .storybook/main.ts
@@ -0,0 +1,21 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": {
+    "name": "@storybook/nextjs",
+    "options": {}
+  },
+  "docs": {
+    "autodocs": true
+  }
+};
+export default config;
