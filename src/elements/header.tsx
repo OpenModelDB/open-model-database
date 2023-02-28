071a75add0408e062272b4159ade040f88b01ecc
Joey Ballentine <joeyjballentine@gmail.com>
2023-02-28 00:48:03 -0500

    remove searchbar

--- src/elements/header.tsx
+++ src/elements/header.tsx
@@ -34,34 +34,6 @@ export function Header() {
 
                     <span className={style.spacer}></span>
 
-                    <section className="relative mx-auto w-full max-w-md rounded-md px-5">
-                        <div className="relative">
-                            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                                <svg
-                                    className="h-5 w-5 text-fade-400"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path
-                                        d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z"
-                                        stroke="currentColor"
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                    ></path>
-                                </svg>
-                            </span>
-
-                            <input
-                                className="w-full rounded-md border bg-white py-3 pl-10 pr-4 text-fade-700 focus:border-accent-400 focus:outline-none focus:ring focus:ring-accent-300 focus:ring-opacity-40 dark:border-fade-600 dark:bg-fade-900 dark:text-fade-300 dark:focus:border-accent-300"
-                                placeholder="Search"
-                                type="text"
-                            />
-                        </div>
-                    </section>
-
-                    <span className={style.spacer}></span>
-
                     <Link
                         external
                         className={style.iconLink}
