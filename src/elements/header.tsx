26aeacea7cf0e6009d62ea45eee6166512d2b3e1
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-02-28 19:17:10 -0500

    Start of the main page stuff (#23)

--- src/elements/header.tsx
+++ src/elements/header.tsx
@@ -5,6 +5,7 @@ import { FaDiscord, FaGithub } from 'react-icons/fa';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 import Logo from '../../public/logo.svg';
 import { toggleColorScheme } from '../lib/color-scheme';
+import { joinClasses } from '../lib/util';
 import { Link } from './components/link';
 import style from './header.module.scss';
 
@@ -22,7 +23,10 @@ export function Header() {
                     </Link>
 
                     <Link
-                        className={style.docLink}
+                        className={joinClasses(
+                            style.docLink,
+                            'transform rounded-lg px-4 py-2 font-medium capitalize tracking-wide text-accent transition-colors duration-300 hover:bg-fade-100 focus:outline-none focus:ring focus:ring-accent-300 focus:ring-opacity-80 dark:text-accent-400 dark:hover:bg-fade-800'
+                        )}
                         href="/docs/faq"
                     >
                         How to upscale
