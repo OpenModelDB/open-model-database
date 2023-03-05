c87cad82b6f48e7c20a393edcf2c34df47757488
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-03-05 10:09:40 -0500

    Various Improvements (#26)
    * Add searchbar
    
    * Add arch to cards
    
    * more stuff
    
    * empty models list placeholder
    
    * Combine blocks, add ALL tag
    
    * always scroll
    
    * ...
    
    * consistencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,30 @@ interface Props {
     modelData: Record<ModelId, Model>;
 }
 
+const startsWithVowel = (str: string) => {
+    const firstLetter = str[0].toLowerCase();
+    return ['a', 'e', 'i', 'o', 'u'].includes(firstLetter);
+};
+
 export default function Page({ modelIds, modelData }: Props) {
     const allTags = new Set<string>();
     modelIds.forEach((id) => {
         modelData[id].tags.forEach((tag) => allTags.add(tag));
     });
     const [selectedTag, setSelectedTag] = useState<TagId>();
+
+    const [searchQuery, setSearchQuery] = useState<string>();
+
+    const availableModels = modelIds
+        .filter((id) => (selectedTag ? modelData[id].tags.includes(selectedTag) : true))
+        .filter((id) => {
+            const { name, architecture, author, scale } = modelData[id];
+            return searchQuery
+                ? `${id} ${name} ${architecture} ${scale}x ${String(author)}`
+                      .toLowerCase()
+                      .includes(searchQuery.toLowerCase())
+                : true;
+        });
 
     return (
         <>
@@ -37,9 +55,9 @@ export default function Page({ modelIds, modelData }: Props) {
                 />
             </Head>
             <PageContainer>
-                <div className="py-4 sm:py-4 lg:py-6">
+                <div className="py-6">
                     <div className="mx-auto max-w-screen-2xl">
-                        <div className="rounded-lg bg-fade-100 p-4 dark:bg-fade-800 md:py-6 lg:py-8">
+                        <div className="rounded-lg bg-fade-100 p-4 dark:bg-fade-800">
                             <h1 className="mb-4 text-center text-2xl font-bold capitalize text-accent-500 dark:text-gray-200 md:mb-6 lg:text-3xl">
                                 The best place to find AI Upscaling models
                             </h1>
@@ -52,20 +70,50 @@ export default function Page({ modelIds, modelData }: Props) {
                             <p className="mx-auto max-w-screen-md text-center text-gray-500 md:text-lg">
                                 Currently listing <a className="font-bold text-accent-500">{modelIds.length}</a> models.
                             </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="py-4 sm:py-4 lg:py-6">
-                    <div className="mx-auto max-w-screen-2xl">
-                        <div className="rounded-lg bg-fade-100 p-4 dark:bg-fade-800 md:py-2 lg:py-4">
-                            <h1 className="mb-6 text-center text-2xl font-bold capitalize text-accent-500 dark:text-gray-200 lg:text-3xl">
-                                Models
-                            </h1>
-                            <div className="mb-3 flex flex-row flex-wrap place-content-center justify-items-center align-middle">
+
+                            {/* Search */}
+                            <div className="relative mb-4 flex h-10 w-full">
+                                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                                    <svg
+                                        aria-hidden="true"
+                                        className="h-5 w-5 text-gray-500 dark:text-gray-400"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                        <path
+                                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                        />
+                                    </svg>
+                                </div>
+                                <input
+                                    className="w-full rounded-lg border border-solid border-gray-300 bg-white px-4 py-2 pl-10 text-gray-700 shadow-sm focus:border-accent-500 focus:outline-none focus:ring-1 focus:ring-accent-500 dark:border-gray-700 dark:bg-fade-900 dark:text-gray-100 dark:focus:border-accent-500 dark:focus:ring-accent-500"
+                                    placeholder="Search"
+                                    type="text"
+                                    value={searchQuery}
+                                    onChange={(e) => setSearchQuery(e.target.value)}
+                                />
+                            </div>
+
+                            {/* Tags */}
+                            <div className="mb-2 flex flex-row flex-wrap place-content-center justify-items-center align-middle">
+                                <div
+                                    className={joinClasses(
+                                        'mr-2 mb-2 w-fit cursor-pointer rounded-lg bg-gray-200 px-2 py-1 text-sm font-medium uppercase text-gray-800 transition-colors ease-in-out hover:bg-fade-500 hover:text-gray-100 dark:bg-gray-700 dark:text-gray-100 dark:hover:bg-fade-500',
+                                        !selectedTag && 'bg-accent-500 text-gray-100 dark:bg-accent-500 '
+                                    )}
+                                    onClick={() => setSelectedTag(undefined)}
+                                >
+                                    All
+                                </div>
                                 {Array.from(allTags).map((tag) => (
                                     <div
                                         className={joinClasses(
-                                            'mr-2 mb-2 w-fit cursor-pointer rounded-full bg-gray-200 px-2 py-1 text-sm font-medium text-gray-800 transition-colors ease-in-out hover:bg-accent-500 hover:text-gray-100 dark:bg-gray-700 dark:text-gray-100 dark:hover:bg-fade-500',
+                                            'mr-2 mb-2 w-fit cursor-pointer rounded-lg bg-gray-200 px-2 py-1 text-sm font-medium uppercase text-gray-800 transition-colors ease-in-out hover:bg-fade-500 hover:text-gray-100 dark:bg-gray-700 dark:text-gray-100 dark:hover:bg-fade-500',
                                             selectedTag == tag && 'bg-accent-500 text-gray-100 dark:bg-accent-500 '
                                         )}
                                         key={tag}
@@ -75,10 +123,11 @@ export default function Page({ modelIds, modelData }: Props) {
                                     </div>
                                 ))}
                             </div>
-                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-                                {modelIds
-                                    .filter((id) => (selectedTag ? modelData[id].tags.includes(selectedTag) : true))
-                                    .map((id) => {
+                            {/* Model Cards */}
+                            {availableModels.length > 0 ? (
+                                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                                    {availableModels.map((id) => {
+                                        const { architecture, author, scale } = modelData[id];
                                         const category = (modelData[id].description.split('\n')[0] ?? '').replace(
                                             'Category: ',
                                             ''
@@ -91,28 +140,64 @@ export default function Page({ modelIds, modelData }: Props) {
 
                                         return (
                                             <div
-                                                className="rounded-lg bg-white shadow-lg dark:bg-fade-900"
+                                                className="transform overflow-hidden rounded-lg border border-solid border-gray-300 bg-white shadow-lg hover:shadow-xl dark:border-gray-700 dark:bg-fade-900"
                                                 key={id}
                                             >
+                                                <div className="m-0 -mb-2 w-full p-0">
+                                                    <a
+                                                        className="relative block"
+                                                        href={`/models/${id}`}
+                                                        rel="noreferrer"
+                                                        target="_blank"
+                                                    >
+                                                        {/* Arch tag on image */}
+                                                        <div className="absolute top-0 right-0 m-2">
+                                                            <div className="flex flex-row flex-wrap place-content-center justify-items-center gap-x-2 align-middle">
+                                                                <div className="cursor-pointer rounded-lg bg-accent-500 px-2 py-1 text-sm font-medium text-gray-100 transition-colors ease-in-out hover:bg-accent-600 hover:text-gray-100 dark:bg-accent-600 dark:text-gray-100 dark:hover:bg-accent-700">
+                                                                    {architecture}
+                                                                </div>
+                                                                <div className="cursor-pointer rounded-lg bg-accent-500 px-2 py-1 text-sm font-medium text-gray-100 transition-colors ease-in-out hover:bg-accent-600 hover:text-gray-100 dark:bg-accent-600 dark:text-gray-100 dark:hover:bg-accent-700">
+                                                                    {scale}x
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        {/* eslint-disable-next-line @next/next/no-img-element */}
+                                                        <img
+                                                            alt="img"
+                                                            className={joinClasses(
+                                                                'w-full overflow-hidden object-cover'
+                                                            )}
+                                                            src={`https://picsum.photos/512/256`}
+                                                        />
+                                                    </a>
+                                                </div>
                                                 <div className="p-4">
                                                     <Link href={`/models/${id}`}>
                                                         <div className="block text-2xl font-bold text-gray-800 dark:text-gray-100">
                                                             {id}
                                                         </div>
                                                     </Link>
-                                                    <div className="mt-2 text-gray-600 dark:text-gray-400">
-                                                        {asArray(modelData[id].author).map((userId) => (
-                                                            <React.Fragment key={userId}>
-                                                                <Link href={`/users/${userId}`}>
-                                                                    <div className="flex">
-                                                                        <div className="mr-1">by</div>
+                                                    <div className="text-gray-600 dark:text-gray-400">
+                                                        <div className="flex">
+                                                            <div className="mr-1">
+                                                                {startsWithVowel(architecture) ? 'an' : 'a'}
+                                                            </div>
+                                                            <Link href={`/architectures/${architecture}`}>
+                                                                <div className="mr-1 font-bold text-accent-500">
+                                                                    {architecture}
+                                                                </div>
+                                                            </Link>
+                                                            <div className="mr-1">model by</div>
+                                                            {asArray(author).map((userId) => (
+                                                                <React.Fragment key={userId}>
+                                                                    <Link href={`/users/${userId}`}>
                                                                         <div className="font-bold text-accent-500">
                                                                             {userId}
                                                                         </div>
-                                                                    </div>
-                                                                </Link>
-                                                            </React.Fragment>
-                                                        ))}
+                                                                    </Link>
+                                                                </React.Fragment>
+                                                            ))}
+                                                        </div>
                                                     </div>
 
                                                     {/* Description */}
@@ -129,7 +214,7 @@ export default function Page({ modelIds, modelData }: Props) {
                                                     <div className="mt-2 flex flex-row flex-wrap">
                                                         {tags.map((tag) => (
                                                             <div
-                                                                className="mr-2 mb-2 rounded-full bg-gray-200 px-2 py-1 text-sm font-medium text-gray-800 dark:bg-gray-700 dark:text-gray-100"
+                                                                className="mr-2 mb-1 rounded-lg bg-gray-200 px-2 py-1 text-sm font-medium uppercase text-gray-800 dark:bg-gray-700 dark:text-gray-100"
                                                                 key={tag}
                                                             >
                                                                 {tag}
@@ -140,7 +225,17 @@ export default function Page({ modelIds, modelData }: Props) {
                                             </div>
                                         );
                                     })}
-                            </div>
+                                </div>
+                            ) : (
+                                <div className="flex flex-col items-center justify-center p-6">
+                                    <div className="text-2xl font-bold text-accent-500 dark:text-gray-100">
+                                        No models found
+                                    </div>
+                                    <div className="text-gray-500 dark:text-gray-400">
+                                        Try changing your search filters
+                                    </div>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
