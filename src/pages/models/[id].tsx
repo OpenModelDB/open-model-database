c44f0310fb8a65284d3bda4d393141341a93d201
Joey Ballentine <joeyjballentine@gmail.com>
2023-03-05 20:37:36 -0500

    Some extra stuff

--- src/pages/models/[id].tsx
+++ src/pages/models/[id].tsx
@@ -1,10 +1,13 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import { ParsedUrlQuery } from 'querystring';
+import React from 'react';
 import { FiExternalLink } from 'react-icons/fi';
 import { PageContainer } from '../../elements/page';
 import { Model, ModelId } from '../../lib/schema';
 import { getAllModelIds, getModelData } from '../../lib/server/data';
+import { asArray } from '../../lib/util';
 
 interface Params extends ParsedUrlQuery {
     id: ModelId;
@@ -13,6 +16,34 @@ interface Props {
     modelId: ModelId;
     modelData: Model;
 }
+
+const renderTags = (tags: string[]) => (
+    <div className="flex flex-row flex-wrap gap-2">
+        {tags.map((tag) => {
+            return (
+                <span
+                    className="inline-flex items-center rounded-full bg-fade-100 px-2.5 py-0.5 text-xs font-medium text-fade-800 dark:bg-fade-800 dark:text-fade-200"
+                    key={tag}
+                >
+                    {tag}
+                </span>
+            );
+        })}
+    </div>
+);
+
+const getColorMode = (numberOfChannels: number) => {
+    switch (numberOfChannels) {
+        case 1:
+            return 'grayscale';
+        case 3:
+            return 'rgb';
+        case 4:
+            return 'rgba';
+        default:
+            return numberOfChannels;
+    }
+};
 
 export default function Page({ modelId, modelData }: Props) {
     console.log('🚀 ~ file: [id].tsx:17 ~ Page ~ modelData:', modelData);
@@ -34,12 +65,6 @@ export default function Page({ modelId, modelData }: Props) {
                 />
             </Head>
             <PageContainer>
-                {/* <div>
-                    <p>{modelId}</p>
-                    <p>{modelData.name}</p>
-                    <br />
-                    <pre>{JSON.stringify(modelData, undefined, 4)}</pre>
-                </div> */}
                 {/* Two columns */}
                 <div className="grid w-full grid-cols-3 gap-4 py-6">
                     {/* Left column */}
@@ -48,7 +73,18 @@ export default function Page({ modelId, modelData }: Props) {
                         <div className="">
                             <div>
                                 <h1 className="m-0">{modelData.name}</h1>
-                                <h3 className="m-0">by {modelData.author}</h3>
+                                <p className="m-0">
+                                    by{' '}
+                                    <a>
+                                        <strong className="m-0 text-lg text-accent-600 dark:text-accent-500">
+                                            {asArray(modelData.author).map((userId) => (
+                                                <React.Fragment key={userId}>
+                                                    <Link href={`/users/${userId}`}>{userId}</Link>
+                                                </React.Fragment>
+                                            ))}
+                                        </strong>
+                                    </a>
+                                </p>
                             </div>
                             <div>
                                 <p className="whitespace-pre-line">{modelData.description}</p>
@@ -58,58 +94,60 @@ export default function Page({ modelId, modelData }: Props) {
                     {/* Right column */}
                     <div className="col-span-1 w-full">
                         {/* Download Button */}
-                        {modelData.resources.map((resource) => {
-                            return resource.urls.map((url) => {
-                                const isExternal = !url.includes('oracle');
-                                let host = 'unknown';
-                                if (url.includes('github')) {
-                                    host = 'GitHub';
-                                } else if (url.includes('drive.google')) {
-                                    host = 'Google Drive';
-                                } else if (url.includes('mega.nz')) {
-                                    host = 'Mega';
-                                }
+                        <div className="mb-2">
+                            {modelData.resources.map((resource) => {
+                                return resource.urls.map((url) => {
+                                    const isExternal = !url.includes('oracle');
+                                    let host = 'unknown';
+                                    if (url.includes('github')) {
+                                        host = 'GitHub';
+                                    } else if (url.includes('drive.google')) {
+                                        host = 'Google Drive';
+                                    } else if (url.includes('mega.nz')) {
+                                        host = 'Mega';
+                                    }
 
-                                return (
-                                    <div key={resource.sha256}>
-                                        <button
-                                            className="mr-2 mb-2 inline-flex w-full cursor-pointer items-center rounded-lg border-0 border-accent-700 bg-accent-500 px-5 py-2.5 text-center text-lg font-medium text-white transition duration-100 ease-in-out hover:bg-accent-600 focus:outline-none focus:ring-4 focus:ring-accent-700 dark:focus:ring-accent-500"
-                                            type="button"
-                                            onClick={() => window.open(url, '_blank')}
-                                        >
-                                            <div className="w-full">
-                                                {isExternal ? (
-                                                    <FiExternalLink
-                                                        className="mr-2 h-4 w-4"
-                                                        viewBox="0 0 22 22"
-                                                    />
-                                                ) : (
-                                                    <svg
-                                                        className="mr-2 h-4 w-4 fill-current"
-                                                        viewBox="0 0 20 20"
-                                                        xmlns="http://www.w3.org/2000/svg"
-                                                    >
-                                                        <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
-                                                    </svg>
-                                                )}
-                                                Download{' '}
-                                                {resource.size
-                                                    ? `(${(resource.size / 1024 / 1024).toFixed(1)} MB)`
-                                                    : ''}
+                                    return (
+                                        <div key={resource.sha256}>
+                                            <button
+                                                className="mr-2 mb-2 inline-flex w-full cursor-pointer items-center rounded-lg border-0 border-accent-700 bg-accent-500 px-5 py-2.5 text-center text-lg font-medium text-white transition duration-100 ease-in-out hover:bg-accent-600 focus:outline-none focus:ring-4 focus:ring-accent-700 dark:focus:ring-accent-500"
+                                                type="button"
+                                                onClick={() => window.open(url, '_blank')}
+                                            >
+                                                <div className="w-full">
+                                                    {isExternal ? (
+                                                        <FiExternalLink
+                                                            className="mr-2 h-4 w-4"
+                                                            viewBox="0 0 22 22"
+                                                        />
+                                                    ) : (
+                                                        <svg
+                                                            className="mr-2 h-4 w-4 fill-current"
+                                                            viewBox="0 0 20 20"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                        >
+                                                            <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                                                        </svg>
+                                                    )}
+                                                    Download{' '}
+                                                    {resource.size
+                                                        ? `(${(resource.size / 1024 / 1024).toFixed(1)} MB)`
+                                                        : ''}
+                                                </div>
+                                            </button>
+                                            <div className="w-full text-center">
+                                                {isExternal ? `Hosted offsite by ${host}` : 'Hosted by OpenModelDB'}
                                             </div>
-                                        </button>
-                                        <div className="w-full text-center">
-                                            {isExternal ? `Hosted offsite by ${host}` : 'Hosted by OpenModelDB'}
                                         </div>
-                                    </div>
-                                );
-                            });
-                        })}
+                                    );
+                                });
+                            })}
+                        </div>
 
-                        <div className="relative table-auto overflow-hidden rounded-lg">
-                            <table className="w-full overflow-hidden rounded-lg text-left text-sm text-gray-500 dark:text-gray-400">
-                                <tbody className="overflow-hidden rounded-lg">
-                                    <tr className="border-b border-fade-200 dark:border-fade-600">
+                        <div className="relative table-auto overflow-hidden rounded-lg border-fade-700">
+                            <table className="w-full border-collapse overflow-hidden rounded-lg border-fade-700 text-left text-sm text-gray-500 dark:text-gray-400 ">
+                                <tbody className="overflow-hidden  rounded-lg ">
+                                    <tr className=" ">
                                         <th
                                             className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium text-gray-900 dark:bg-fade-800 dark:text-white"
                                             scope="row"
@@ -118,7 +156,7 @@ export default function Page({ modelId, modelData }: Props) {
                                         </th>
                                         <td className="px-6 py-4">{modelData.architecture}</td>
                                     </tr>
-                                    <tr className="border-b">
+                                    <tr className="">
                                         <th
                                             className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium text-fade-900 dark:bg-fade-800 dark:text-white"
                                             scope="row"
@@ -127,42 +165,85 @@ export default function Page({ modelId, modelData }: Props) {
                                         </th>
                                         <td className="px-6 py-4 ">{modelData.scale}</td>
                                     </tr>
-                                    <tr className="border-b">
-                                        <th
-                                            className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium text-fade-900 dark:bg-fade-800 dark:text-white"
-                                            scope="row"
-                                        >
-                                            Size
-                                        </th>
-                                        <td className="px-6 py-4">{modelData.size?.join(', ')}</td>
-                                    </tr>
-                                    <tr className="border-b">
+                                    {modelData.size && (
+                                        <tr className="">
+                                            <th
+                                                className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium text-fade-900 dark:bg-fade-800 dark:text-white"
+                                                scope="row"
+                                            >
+                                                Size
+                                            </th>
+                                            <td className="px-6 py-4">{renderTags(modelData.size)}</td>
+                                        </tr>
+                                    )}
+                                    <tr className="">
                                         <th
                                             className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium text-gray-900 dark:bg-fade-800 dark:text-white"
                                             scope="row"
                                         >
                                             Tags
                                         </th>
+                                        <td className="px-6 py-4">{renderTags(modelData.tags)}</td>
+                                    </tr>
+                                    <tr className="">
+                                        <th
+                                            className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium text-gray-900 dark:bg-fade-800 dark:text-white"
+                                            scope="row"
+                                        >
+                                            Color Mode
+                                        </th>
                                         <td className="px-6 py-4">
-                                            <div className="flex flex-row flex-wrap gap-2">
-                                                {modelData.tags.map((tag) => {
-                                                    return (
-                                                        <span
-                                                            className="inline-flex items-center rounded-full bg-fade-100 px-2.5 py-0.5 text-xs font-medium text-fade-800 dark:bg-fade-800 dark:text-fade-200"
-                                                            key={tag}
-                                                        >
-                                                            {tag}
-                                                        </span>
-                                                    );
-                                                })}
+                                            <div className="flex flex-row flex-wrap gap-2 uppercase">
+                                                {getColorMode(modelData.inputChannels)} →{' '}
+                                                {getColorMode(modelData.outputChannels)}
                                             </div>
                                         </td>
                                     </tr>
+                                    {Object.entries(modelData)
+                                        .filter(
+                                            ([key, _value]) =>
+                                                ![
+                                                    // Handled by other parts of page
+                                                    'name',
+                                                    'author',
+                                                    'description',
+                                                    'resources',
+                                                    // Already handled manually
+                                                    'architecture',
+                                                    'scale',
+                                                    'size',
+                                                    'tags',
+                                                    'inputChannels',
+                                                    'outputChannels',
+                                                    // This is just messed up in the data
+                                                    'pretrainedModelG',
+                                                ].includes(key)
+                                        )
+                                        .filter(([_key, value]) => !!value)
+                                        .sort()
+                                        .map(([key, value]) => {
+                                            return (
+                                                <tr
+                                                    className=""
+                                                    key={key}
+                                                >
+                                                    <th
+                                                        className="whitespace-nowrap bg-fade-100 px-6 py-4 font-medium capitalize text-fade-900 dark:bg-fade-800 dark:text-white"
+                                                        scope="row"
+                                                    >
+                                                        {key}
+                                                    </th>
+                                                    <td className="px-6 py-4">
+                                                        {Array.isArray(value)
+                                                            ? renderTags(value.map((v) => String(v)))
+                                                            : value}
+                                                    </td>
+                                                </tr>
+                                            );
+                                        })}
                                 </tbody>
                             </table>
                         </div>
-
-                        {modelData.license}
                     </div>
                 </div>
             </PageContainer>
