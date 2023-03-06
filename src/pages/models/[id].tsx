1605febaead9fbd367f309563b8e3ddd59d23bb6
Joey Ballentine <joeyjballentine@gmail.com>
2023-03-06 00:36:27 -0500

    fix light mode download button

--- src/pages/models/[id].tsx
+++ src/pages/models/[id].tsx
@@ -197,7 +197,7 @@ export default function Page({ modelData }: Props) {
                                     return (
                                         <div key={resource.sha256}>
                                             <button
-                                                className="mr-2 mb-2 inline-flex w-full cursor-pointer items-center rounded-lg border-0 border-accent-700 bg-accent-500 px-5 py-2.5 text-center text-lg font-medium text-white transition duration-100 ease-in-out hover:bg-accent-600 focus:outline-none focus:ring-4 focus:ring-accent-700 dark:focus:ring-accent-500"
+                                                className="mr-2 mb-2 inline-flex w-full cursor-pointer items-center rounded-lg border-0 border-accent-700 bg-accent-600 px-5 py-2.5 text-center text-lg font-medium text-white transition duration-100 ease-in-out hover:bg-accent-500 focus:outline-none focus:ring-4 focus:ring-accent-700 dark:bg-accent-500 dark:hover:bg-accent-600 dark:focus:ring-accent-500"
                                                 type="button"
                                                 onClick={() => window.open(url, '_blank')}
                                             >
