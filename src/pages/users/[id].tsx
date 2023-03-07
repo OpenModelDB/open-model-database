b40d7d06b3e199c9c8b6d850b18bf85e6bb7fa86
Joey Ballentine <joeyjballentine@gmail.com>
2023-03-06 23:43:01 -0500

    Simple user page

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,8 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
-import Link from 'next/link';
 import { ParsedUrlQuery } from 'querystring';
 import React from 'react';
+import { ModelCard } from '../../elements/components/model-card';
+import { PageContainer } from '../../elements/page';
 import { Model, ModelId, User, UserId } from '../../lib/schema';
 import { getAllModelIds, getModelData, getUsers } from '../../lib/server/data';
 
@@ -15,7 +16,7 @@ interface Props {
     models: Record<ModelId, Model>;
 }
 
-export default function Page({ userId, user, models }: Props) {
+export default function Page({ user, models }: Props) {
     return (
         <>
             <Head>
@@ -33,22 +34,68 @@ export default function Page({ userId, user, models }: Props) {
                     rel="icon"
                 />
             </Head>
-            <main>
-                <div>
-                    <p>{userId}</p>
-                    <p>{user.name}</p>
-                    <br />
-                    <p>Models:</p>
-                    <pre>
-                        {Object.keys(models).map((id) => (
-                            <span key={id}>
-                                <Link href={`/models/${id}`}>{id}</Link>
-                                {'\n'}
-                            </span>
-                        ))}
-                    </pre>
+            <PageContainer>
+                <div className="py-6">
+                    <div className="mx-auto max-w-screen-2xl">
+                        <div className="rounded-lg bg-fade-100 p-4 dark:bg-fade-800">
+                            <h1 className="mb-4 text-center text-2xl font-bold text-accent-500 dark:text-fade-200 md:mb-6 lg:text-3xl">
+                                {`${user.name}'s Models`}
+                            </h1>
+
+                            {/* Model Cards */}
+                            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+                                {Object.keys(models).map((id) => {
+                                    const { architecture, author, scale, description, tags } = models[id as ModelId];
+                                    const lines = description.split('\n');
+                                    const descLines: string[] = [];
+                                    let category = '',
+                                        purpose = '',
+                                        pretrained = '',
+                                        dataset = '';
+                                    lines.forEach((line) => {
+                                        if (line.startsWith('Category: ')) {
+                                            category = String(line).replace('Category: ', '');
+                                        } else if (line.startsWith('Purpose: ')) {
+                                            purpose = String(line).replace('Purpose: ', '');
+                                        } else if (line.startsWith('Pretrained: ')) {
+                                            pretrained = String(line).replace('Pretrained: ', '');
+                                        } else if (line.startsWith('Dataset: ')) {
+                                            dataset = String(line).replace('Dataset: ', '');
+                                        } else if (line !== '') {
+                                            descLines.push(line.trim());
+                                        }
+                                    });
+                                    const purposeSentence = category
+                                        ? `A ${scale}x model for ${purpose}.`
+                                        : `A ${scale}x model.`;
+                                    const datasetSentence = dataset
+                                        ? `Trained on ${dataset}.`
+                                        : 'Unknown training dataset.';
+                                    const pretrainedSentence = pretrained
+                                        ? `Pretrained using ${pretrained}.`
+                                        : 'Unknown pretrained model.';
+                                    const actualDescription =
+                                        descLines.length > 0
+                                            ? descLines.join('\n').trim()
+                                            : `${purposeSentence} ${datasetSentence} ${pretrainedSentence}`;
+
+                                    return (
+                                        <ModelCard
+                                            architecture={architecture}
+                                            author={author}
+                                            description={actualDescription}
+                                            id={id}
+                                            key={id}
+                                            scale={scale}
+                                            tags={tags}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </main>
+            </PageContainer>
         </>
     );
 }
