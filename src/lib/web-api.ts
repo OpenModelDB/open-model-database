e11bedee04aa0cea3432c9a230028161718e11df
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-03-07 19:41:50 -0500

    Make model name and description editable (#31)
    * Make model name and description editable
    
    * Refactor into method
    
    * rename method
    
    * Update content after edit
    
    * Make contentEditable work properly
    
    * Don't register update listener without an API
    
    ---------
    
    Co-authored-by: RunDevelopment <mitchi5000.ms@googlemail.com>

--- src/lib/web-api.ts
+++ src/lib/web-api.ts
@@ -1,7 +1,59 @@
-import { DBApi } from './data-api';
+import { CollectionApi, DBApi } from './data-api';
 import { JsonApiCollection, JsonApiRequestHandler, JsonRequest, JsonResponse, Method } from './data-json-api';
-import { Model, ModelId, Tag, TagId, User, UserId } from './schema';
-import { lazy } from './util';
+import { delay, lazy, noop } from './util';
+
+class NotifyOnWrites<Id, Value> implements CollectionApi<Id, Value> {
+    private readonly collection: CollectionApi<Id, Value>;
+    private readonly notify: () => void;
+    constructor(collection: CollectionApi<Id, Value>, notify: () => void) {
+        this.collection = collection;
+        this.notify = notify;
+    }
+
+    get(id: Id): Promise<Value> {
+        return this.collection.get(id);
+    }
+    getIds(): Promise<Id[]> {
+        return this.collection.getIds();
+    }
+    getAll(): Promise<Map<Id, Value>> {
+        return this.collection.getAll();
+    }
+    update(updates: Iterable<readonly [Id, Value]>): Promise<void> {
+        return this.collection.update(updates).then(this.notify);
+    }
+    delete(ids: Iterable<Id>): Promise<void> {
+        return this.collection.delete(ids).then(this.notify);
+    }
+    changeId(from: Id, to: Id): Promise<void> {
+        return this.collection.changeId(from, to).then(this.notify);
+    }
+}
+
+const updateListeners = new Set<() => void>();
+export function addUpdateListener(listener: () => void): () => void {
+    updateListeners.add(listener);
+    return () => {
+        updateListeners.delete(listener);
+    };
+}
+let notifying = false;
+function notifyListeners() {
+    if (notifying) return;
+    notifying = true;
+    delay(20)
+        .then(() => {
+            notifying = false;
+            for (const l of updateListeners) {
+                try {
+                    l();
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+        })
+        .catch(noop);
+}
 
 function createWebRequestHandler<Id, Value>(url: string): JsonApiRequestHandler<Id, Value> {
     return async <M extends Method>(
@@ -16,12 +68,14 @@ function createWebRequestHandler<Id, Value>(url: string): JsonApiRequestHandler<
         return data;
     };
 }
-
+function createWebCollection<Id, Value>(path: string): CollectionApi<Id, Value> {
+    return new NotifyOnWrites(new JsonApiCollection(createWebRequestHandler<Id, Value>(path)), notifyListeners);
+}
 export const getWebApi = lazy(async (): Promise<DBApi | undefined> => {
     const webApi: DBApi = {
-        models: new JsonApiCollection(createWebRequestHandler<ModelId, Model>('/api/models')),
-        tags: new JsonApiCollection(createWebRequestHandler<TagId, Tag>('/api/tags')),
-        users: new JsonApiCollection(createWebRequestHandler<UserId, User>('/api/users')),
+        models: createWebCollection('/api/models'),
+        tags: createWebCollection('/api/tags'),
+        users: createWebCollection('/api/users'),
     };
 
     // we do an empty update to test the waters
