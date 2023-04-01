0f2bb4ca39e3d7eb15cf855e11e359676fcbc558
Michael Schmidt <msrd0000@gmail.com>
2023-04-01 17:18:31 +0200

    Added tag tooltips (#87)
    * Added tag tooltips
    
    * Removed useless class

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.scss';
+import 'react-tooltip/dist/react-tooltip.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
