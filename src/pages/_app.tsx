dd548f90d2274dd047b7e9139644e1f86fd7c979
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-04-03 19:04:12 +0200

    Use context for web api

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.scss';
 import 'react-tooltip/dist/react-tooltip.css';
+import { WebApiProvider } from '../lib/hooks/use-web-api';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <WebApiProvider>
+            <Component {...pageProps} />
+        </WebApiProvider>
+    );
 }
