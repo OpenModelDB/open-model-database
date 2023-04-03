b84bd20ae65d1d3c3f8bd58117390e3c16bdf807
Michael Schmidt <msrd0000@gmail.com>
2023-04-03 16:14:13 +0200

    Improvements to editable users (#102)

--- src/elements/components/editable-users.tsx
+++ src/elements/components/editable-users.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BsPersonFillAdd } from 'react-icons/bs';
 import { MdRemoveCircle } from 'react-icons/md';
 import { useUsers } from '../../lib/hooks/use-users';
+import { joinList } from '../../lib/react-util';
 import { UserId } from '../../lib/schema';
 import { Link } from './link';
 
@@ -15,66 +16,69 @@ export function EditableUsers({ users, onChange, readonly }: EditableUsersProps)
     const { userData } = useUsers();
     const [randomUser] = userData.keys();
 
-    return (
-        <div className="flex flex-row flex-wrap gap-2">
-            <strong className="m-0 flex flex-row flex-wrap gap-2 text-lg text-accent-600 dark:text-accent-500">
-                {users.map((userId, index) => {
-                    if (!readonly && onChange) {
+    if (readonly || !onChange) {
+        return (
+            <>
+                {'by '}
+                {joinList(
+                    users.map((userId) => {
                         return (
-                            <div
-                                className="flex flex-row gap-2"
-                                key={`${userId} ${index}`}
+                            <Link
+                                className="text-lg font-bold text-accent-600 dark:text-accent-400"
+                                href={`/users/${userId}`}
+                                key={userId}
                             >
-                                <select
-                                    value={userId}
-                                    onChange={(event) => {
-                                        const newUsers = [...users];
-                                        newUsers[index] = event.target.value as UserId;
-                                        onChange(newUsers);
-                                    }}
-                                >
-                                    {[...userData].map(([userId, user]) => (
-                                        <option
-                                            key={userId}
-                                            value={userId}
-                                        >
-                                            {user.name}
-                                        </option>
-                                    ))}
-                                </select>
-                                {index > 0 && (
-                                    <button
-                                        onClick={() => {
-                                            const newUsers = [...users];
-                                            newUsers.splice(index, 1);
-                                            onChange(newUsers);
-                                        }}
-                                    >
-                                        <MdRemoveCircle />
-                                    </button>
-                                )}
-                            </div>
+                                {userData.get(userId)?.name ?? `unknown user:${userId}`}
+                            </Link>
                         );
-                    }
-                    return (
-                        <Link
-                            href={`/users/${userId}`}
-                            key={userId}
-                        >
-                            {userData.get(userId)?.name ?? `unknown user:${userId}`}
-                        </Link>
-                    );
-                })}
-                {!readonly && onChange && (
-                    <button
-                        onClick={() => {
-                            onChange([...users, randomUser]);
-                        }}
-                    >
-                        <BsPersonFillAdd />
-                    </button>
+                    })
                 )}
-            </strong>
+            </>
+        );
+    }
+
+    return (
+        <div className="flex flex-row flex-wrap gap-2 text-lg">
+            {users.map((userId, index) => {
+                return (
+                    <div
+                        className="flex flex-row gap-1"
+                        key={`${userId} ${index}`}
+                    >
+                        <select
+                            value={userId}
+                            onChange={(event) => {
+                                const newUsers = [...users];
+                                newUsers[index] = event.target.value as UserId;
+                                onChange(newUsers);
+                            }}
+                        >
+                            {[...userData].map(([userId, user]) => (
+                                <option
+                                    key={userId}
+                                    value={userId}
+                                >
+                                    {user.name}
+                                </option>
+                            ))}
+                        </select>
+                        {index > 0 && (
+                            <button
+                                onClick={() => {
+                                    const newUsers = [...users];
+                                    newUsers.splice(index, 1);
+                                    onChange(newUsers);
+                                }}
+                            >
+                                <MdRemoveCircle />
+                            </button>
+                        )}
+                    </div>
+                );
+            })}
+            <button onClick={() => onChange([...users, randomUser])}>
+                <BsPersonFillAdd />
+            </button>
         </div>
     );
 }
