a408f79febb32d6ad68494175eddc0869d8e4f48
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-04-16 15:52:50 -0400

    Add Model button (#130)

--- src/elements/header.tsx
+++ src/elements/header.tsx
@@ -1,17 +1,21 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from 'next/router';
 import React from 'react';
 import { FaDiscord, FaGithub } from 'react-icons/fa';
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 import Logo from '../../public/logo.svg';
 import { toggleColorScheme } from '../lib/color-scheme';
-import { useEditModeToggle } from '../lib/hooks/use-web-api';
+import { useEditModeToggle, useWebApi } from '../lib/hooks/use-web-api';
+import { ModelId } from '../lib/schema';
 import { joinClasses } from '../lib/util';
 import { Link } from './components/link';
 import style from './header.module.scss';
 
 export function Header() {
     const { editModeAvailable, editMode, toggleEditMode } = useEditModeToggle();
+    const router = useRouter();
+    const { webApi } = useWebApi();
 
     return (
         <>
@@ -34,6 +38,62 @@ export function Header() {
                     >
                         How To Upscale
                     </Link>
+                    {editMode && webApi && (
+                        <button
+                            className={joinClasses(
+                                style.docLink,
+                                'bg-transparent font-medium tracking-wide text-accent hover:bg-fade-100 dark:text-accent-400 dark:hover:bg-fade-800'
+                            )}
+                            onClick={() => {
+                                (async () => {
+                                    const modelId = prompt('Enter model ID');
+                                    if (!modelId) {
+                                        return;
+                                    }
+                                    const arches = await webApi.architectures.getAll();
+                                    if (!arches.size) {
+                                        return;
+                                    }
+                                    const firstArch = [...arches.entries()][0][0];
+                                    webApi.models
+                                        .update([
+                                            [
+                                                modelId as ModelId,
+                                                {
+                                                    name: modelId,
+                                                    author: [],
+                                                    license: null,
+                                                    tags: [],
+                                                    description: '',
+                                                    date: new Date().toISOString().split('T')[0],
+                                                    architecture: firstArch,
+                                                    size: null,
+                                                    scale: 4,
+                                                    inputChannels: 3,
+                                                    outputChannels: 3,
+                                                    resources: [],
+                                                    images: [],
+                                                },
+                                            ],
+                                        ])
+                                        .then(() => {
+                                            setTimeout(() => {
+                                                router.push(`/models/${modelId}`).catch((error) => {
+                                                    console.error(error);
+                                                });
+                                            }, 1000);
+                                        })
+                                        .catch((error) => {
+                                            console.error(error);
+                                        });
+                                })().catch((error) => {
+                                    console.error(error);
+                                });
+                            }}
+                        >
+                            Add Model
+                        </button>
+                    )}
 
                     <span className={style.spacer} />
 
