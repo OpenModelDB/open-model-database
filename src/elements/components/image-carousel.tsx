f2265d3d6bfa19f1f918d1704a021f6961d32c63
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-04-16 11:56:35 -0400

    Fix image carousel overflow (#131)

--- src/elements/components/image-carousel.tsx
+++ src/elements/components/image-carousel.tsx
@@ -8,6 +8,7 @@ import {
     BsFillTrashFill,
     BsPlusLg,
 } from 'react-icons/bs';
+import { FiMoreHorizontal } from 'react-icons/fi';
 import { Image } from '../../lib/schema';
 import { joinClasses } from '../../lib/util';
 import { EditImageButton } from './image-carousel-edit-popover';
@@ -19,10 +20,16 @@ type ImageCarouselProps = {
     onChange?: (images: Image[]) => void;
 };
 
+const NUMBER_OF_SHOWABLE_IMAGES = 12;
+
 export const ImageCarousel = ({ images, readonly, onChange }: ImageCarouselProps) => {
     const [imageIndex, setImageIndex] = useState(0);
 
     const selectedImage = images[imageIndex] as Image | undefined;
+
+    const page = Math.floor(imageIndex / NUMBER_OF_SHOWABLE_IMAGES);
+    const sliceStartIndex = page * NUMBER_OF_SHOWABLE_IMAGES;
+    const sliceEndIndex = Math.min(sliceStartIndex + NUMBER_OF_SHOWABLE_IMAGES, images.length);
 
     return (
         <div className="relative rounded-lg">
@@ -45,91 +52,104 @@ export const ImageCarousel = ({ images, readonly, onChange }: ImageCarouselProps
                     <BsCaretLeftFill />
                 </button>
                 <div className="flex grow items-center justify-center justify-items-center gap-2 align-middle">
-                    {images.map((image, index) => (
-                        <div
-                            className="flex flex-col items-center"
-                            key={image.type === 'paired' ? image.SR : image.url}
-                        >
-                            {!readonly && (
-                                <div className="flex flex-row items-center">
-                                    <EditImageButton
-                                        image={image}
-                                        onChange={(editedImage) => {
-                                            const newImages = [...images];
-                                            newImages[index] = editedImage;
-                                            if (onChange) {
-                                                onChange(newImages);
-                                            }
-                                        }}
-                                    >
-                                        <AiFillEdit />
-                                    </EditImageButton>
-                                    <button
-                                        className="block"
-                                        onClick={() => {
-                                            const newImages = [...images];
-                                            // Remove image
-                                            newImages.splice(index, 1);
-                                            if (onChange) {
-                                                onChange(newImages);
-                                            }
-                                        }}
-                                    >
-                                        <BsFillTrashFill />
-                                    </button>
-                                </div>
-                            )}
-                            <div
-                                className={joinClasses(
-                                    'border-3 m-0 flex h-12 w-12 cursor-pointer rounded-lg border-solid p-0 transition duration-100 ease-in-out',
-                                    index === imageIndex
-                                        ? 'border-accent-500'
-                                        : 'border-fade-200 hover:border-fade-300 dark:border-fade-700 dark:hover:border-fade-600'
-                                )}
-                                onClick={() => {
-                                    setImageIndex(index);
-                                }}
-                            >
-                                {/* eslint-disable-next-line @next/next/no-img-element */}
-                                <img
-                                    alt="Thumbnail"
-                                    className="m-auto h-auto max-h-full w-auto max-w-full"
-                                    src={image.type === 'paired' ? image.SR : image.url}
-                                />
-                            </div>
-                            {!readonly && (
-                                <div className="flex flex-row items-center">
-                                    <button
-                                        onClick={() => {
-                                            const newImages = [...images];
-                                            // Move image to the left
-                                            newImages.splice(index, 1);
-                                            newImages.splice(index - 1, 0, image);
-                                            if (onChange) {
-                                                onChange(newImages);
-                                            }
-                                        }}
-                                    >
-                                        <BsChevronLeft />
-                                    </button>
-
-                                    <button
-                                        onClick={() => {
-                                            const newImages = [...images];
-                                            // Move image to the right
-                                            newImages.splice(index, 1);
-                                            newImages.splice(index + 1, 0, image);
-                                            if (onChange) {
-                                                onChange(newImages);
-                                            }
-                                        }}
-                                    >
-                                        <BsChevronRight />
-                                    </button>
-                                </div>
-                            )}
+                    {sliceStartIndex > 0 && (
+                        <div>
+                            <FiMoreHorizontal />
                         </div>
-                    ))}
+                    )}
+                    {images.slice(sliceStartIndex, sliceEndIndex).map((image, actualIndex) => {
+                        const index = sliceStartIndex + actualIndex;
+                        return (
+                            <div
+                                className="flex flex-col items-center"
+                                key={image.type === 'paired' ? image.SR : image.url}
+                            >
+                                {!readonly && (
+                                    <div className="flex flex-row items-center">
+                                        <EditImageButton
+                                            image={image}
+                                            onChange={(editedImage) => {
+                                                const newImages = [...images];
+                                                newImages[index] = editedImage;
+                                                if (onChange) {
+                                                    onChange(newImages);
+                                                }
+                                            }}
+                                        >
+                                            <AiFillEdit />
+                                        </EditImageButton>
+                                        <button
+                                            className="block"
+                                            onClick={() => {
+                                                const newImages = [...images];
+                                                // Remove image
+                                                newImages.splice(index, 1);
+                                                if (onChange) {
+                                                    onChange(newImages);
+                                                }
+                                            }}
+                                        >
+                                            <BsFillTrashFill />
+                                        </button>
+                                    </div>
+                                )}
+                                <div
+                                    className={joinClasses(
+                                        'border-3 m-0 flex h-12 w-12 cursor-pointer rounded-lg border-solid p-0 transition duration-100 ease-in-out',
+                                        index === imageIndex
+                                            ? 'border-accent-500'
+                                            : 'border-fade-200 hover:border-fade-300 dark:border-fade-700 dark:hover:border-fade-600'
+                                    )}
+                                    onClick={() => {
+                                        setImageIndex(index);
+                                    }}
+                                >
+                                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                                    <img
+                                        alt="Thumbnail"
+                                        className="m-auto h-auto max-h-full w-auto max-w-full"
+                                        src={image.type === 'paired' ? image.SR : image.url}
+                                    />
+                                </div>
+                                {!readonly && (
+                                    <div className="flex flex-row items-center">
+                                        <button
+                                            onClick={() => {
+                                                const newImages = [...images];
+                                                // Move image to the left
+                                                newImages.splice(index, 1);
+                                                newImages.splice(index - 1, 0, image);
+                                                if (onChange) {
+                                                    onChange(newImages);
+                                                }
+                                            }}
+                                        >
+                                            <BsChevronLeft />
+                                        </button>
+
+                                        <button
+                                            onClick={() => {
+                                                const newImages = [...images];
+                                                // Move image to the right
+                                                newImages.splice(index, 1);
+                                                newImages.splice(index + 1, 0, image);
+                                                if (onChange) {
+                                                    onChange(newImages);
+                                                }
+                                            }}
+                                        >
+                                            <BsChevronRight />
+                                        </button>
+                                    </div>
+                                )}
+                            </div>
+                        );
+                    })}
+                    {sliceEndIndex < images.length && (
+                        <div>
+                            <FiMoreHorizontal />
+                        </div>
+                    )}
                     {!readonly && (
                         <EditImageButton
                             onChange={(newImage) => {
