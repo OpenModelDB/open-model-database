86d61d678a8a99ccfde28f856aa299360da5983f
Michael Schmidt <msrd0000@gmail.com>
2023-04-23 16:47:39 +0200

    Minor design improvements (#148)

--- src/elements/page.tsx
+++ src/elements/page.tsx
@@ -4,13 +4,20 @@ import style from './page.module.scss';
 
 interface PageContainerProps {
     children?: ReactNode;
+    wrapper?: boolean;
 }
 
-export function PageContainer({ children }: PageContainerProps) {
+export function PageContainer({ children, wrapper }: PageContainerProps) {
     return (
         <div className={style.container}>
             <Header />
-            <main className={style.main}>{children}</main>
+            {wrapper ? (
+                <main className={`${style.main} p-0 lg:p-4`}>
+                    <div className="mb-6 bg-fade-100 p-4 dark:bg-fade-800 lg:rounded-lg">{children}</div>
+                </main>
+            ) : (
+                <main className={`${style.main} p-4`}>{children}</main>
+            )}
         </div>
     );
 }
