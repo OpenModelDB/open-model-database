e331f3db58887618d46bd4236512e3291e062787
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-04-24 13:43:45 +0200

    BG color transition

--- src/elements/page.tsx
+++ src/elements/page.tsx
@@ -14,7 +14,9 @@ export function PageContainer({ children, scrollToTop, wrapper }: PageContainerP
             <Header />
             {wrapper ? (
                 <main className={`${style.main} p-0 lg:p-4`}>
-                    <div className="mb-6 bg-fade-100 p-4 dark:bg-fade-800 lg:rounded-lg">{children}</div>
+                    <div className={`${style.wrapper} mb-6 bg-fade-100 p-4 dark:bg-fade-800 lg:rounded-lg`}>
+                        {children}
+                    </div>
                 </main>
             ) : (
                 <main className={`${style.main} p-4`}>{children}</main>
