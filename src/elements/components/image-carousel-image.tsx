6b6b8eed0c8de1c4aa19643fee24b75a7e2e85fe
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2023-04-28 12:21:57 -0400

    Better comparison box (#161)

--- src/elements/components/image-carousel-image.tsx
+++ src/elements/components/image-carousel-image.tsx
@@ -1,5 +1,6 @@
-import { ReactCompareSlider, ReactCompareSliderHandle, ReactCompareSliderImage } from 'react-compare-slider';
 import { Image } from '../../lib/schema';
+import { ImageComparison } from './carousel/comparison';
+import { ImageStandalone } from './carousel/standalone';
 
 type ImageCarouselImageProps = {
     image: Image;
@@ -8,48 +9,9 @@ type ImageCarouselImageProps = {
 export const ImageCarouselImage = ({ image }: ImageCarouselImageProps) => {
     switch (image.type) {
         case 'paired':
-            return (
-                <ReactCompareSlider
-                    className="w-full rounded-lg"
-                    handle={
-                        <ReactCompareSliderHandle
-                            buttonStyle={{
-                                height: '48px',
-                                width: '12px',
-                                borderRadius: '1rem',
-                                backdropFilter: undefined,
-                                background: 'white',
-                                border: 0,
-                                color: 'transparent',
-                                overflow: 'hidden',
-                            }}
-                        />
-                    }
-                    itemOne={
-                        <ReactCompareSliderImage
-                            alt="LR"
-                            className="rendering-pixelated h-full w-full object-scale-down"
-                            src={image.LR}
-                        />
-                    }
-                    itemTwo={
-                        <ReactCompareSliderImage
-                            alt="HR"
-                            className="rendering-pixelated h-full w-full object-scale-down"
-                            src={image.SR}
-                        />
-                    }
-                />
-            );
+            return <ImageComparison image={image} />;
         case 'standalone':
-            return (
-                // eslint-disable-next-line @next/next/no-img-element
-                <img
-                    alt="image"
-                    className="h-full w-full object-scale-down"
-                    src={image.url}
-                />
-            );
+            return <ImageStandalone image={image} />;
         default:
             return null;
     }
