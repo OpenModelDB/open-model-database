4bd4e3270d970183b1c6bc29e07224eb8802a666
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-05-21 18:39:17 +0200

    Ensure integer scaling for model card image pairs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.scss';
 import 'react-tooltip/dist/react-tooltip.css';
+import { DevicePixelRatioProvider } from '../lib/hooks/use-device-pixel-ratio';
 import { WebApiProvider } from '../lib/hooks/use-web-api';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <WebApiProvider>
-            <Component {...pageProps} />
+            <DevicePixelRatioProvider>
+                <Component {...pageProps} />
+            </DevicePixelRatioProvider>
         </WebApiProvider>
     );
 }
