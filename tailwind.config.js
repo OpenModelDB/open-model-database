86963ee93475dabfd6144aa6947e4771f85dd699
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-10-05 17:39:40 +0200

    Fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,9 @@ module.exports = {
             },
         },
         screens: {
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
             xl: '1400px',
         },
     },
