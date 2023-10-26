4a786f16f5c3483f499e5fdb10013a2f5c797601
Michael Schmidt <msrd0000@gmail.com>
2023-10-26 16:01:56 +0200

    Use the `async-mutex` library (#321)

--- src/lib/lock.ts
+++ src/lib/lock.ts
@@ -1,25 +1,4 @@
-class Mutex {
-    private locked = false;
-    private pending: (() => void)[] = [];
-    lock(): Promise<void> {
-        if (this.locked) {
-            return new Promise((resolve) => {
-                this.pending.push(resolve);
-            });
-        } else {
-            this.locked = true;
-            return Promise.resolve();
-        }
-    }
-    unlock(): void {
-        const next = this.pending.shift();
-        if (next) {
-            next();
-        } else {
-            this.locked = false;
-        }
-    }
-}
+import { Mutex } from 'async-mutex';
 
 export class RWLock {
     private b = 0;
@@ -27,26 +6,32 @@ export class RWLock {
     private g = new Mutex();
 
     private async lockRead() {
-        await this.r.lock();
-        this.b++;
-        if (this.b === 1) {
-            await this.g.lock();
+        const release = await this.r.acquire();
+        try {
+            this.b++;
+            if (this.b === 1) {
+                await this.g.acquire();
+            }
+        } finally {
+            release();
         }
-        this.r.unlock();
     }
     private async unlockRead() {
-        await this.r.lock();
-        this.b--;
-        if (this.b === 0) {
-            this.g.unlock();
+        const release = await this.r.acquire();
+        try {
+            this.b--;
+            if (this.b === 0) {
+                this.g.release();
+            }
+        } finally {
+            release();
         }
-        this.r.unlock();
     }
     private async lockWrite() {
-        await this.g.lock();
+        await this.g.acquire();
     }
     private unlockWrite() {
-        this.g.unlock();
+        this.g.release();
     }
 
     async read<T>(operation: () => Promise<T>): Promise<T> {
