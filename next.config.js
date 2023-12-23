4802e05e4248d2cc3ff8c57ff7a2f7476dcca09d
Michael Schmidt <msrd0000@gmail.com>
2023-12-23 14:56:35 +0100

    Fixed the build after next v14 (#341)

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     reactStrictMode: true,
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
