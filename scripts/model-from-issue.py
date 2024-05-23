1be1e6783b1bb22be68da7f1916f107cc906f7b9
Joey Ballentine <joeyjballentine@gmail.com>
2024-05-23 01:17:47 -0400

    Add mode for converting all models at once

--- scripts/model-from-issue.py
+++ scripts/model-from-issue.py
@@ -13,36 +13,63 @@ def fetch_issue(issue_number: int):
     return None
 
 
+def fetch_all_issues():
+    url = "https://api.github.com/repos/OpenModelDB/open-model-database/issues"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return json.loads(response.content)
+
+    return None
+
+
 # parse args
 parser = argparse.ArgumentParser(
     description="Convert a model request issue from OpenModelDB's GitHub to a model file."
 )
 
 parser.add_argument(
-    "issue_number", type=int, help="The issue number to fetch the model request from."
+    "issue_number",
+    type=int,
+    help="The issue number to fetch the model request from.",
+    nargs="?",
 )
 
 args = parser.parse_args()
 
-issue = fetch_issue(args.issue_number)
-if issue is None:
-    print(f"Failed to fetch issue {args.issue_number}")
-    exit(1)
-issue_body = issue["body"]
 
-if issue_body.startswith("```") and issue_body.endswith("```"):
-    issue_body = issue_body[3:-3]
+def convert_issue_to_model(issue):
+    issue_body = issue["body"]
 
-if issue_body.startswith("json"):
-    issue_body = issue_body[4:]
+    if issue_body.startswith("```") and issue_body.endswith("```"):
+        issue_body = issue_body[3:-3]
 
-issue_body = issue_body.strip()
-issue_body = json.loads(issue_body)
+    if issue_body.startswith("json"):
+        issue_body = issue_body[4:]
 
-issue_title = issue["title"]
-model_name = issue_title.replace("[MODEL ADD REQUEST] ", "").replace("-", "_")
+    issue_body = issue_body.strip()
+    issue_body = json.loads(issue_body)
 
-models_dir = "./data/models"
+    issue_title = issue["title"]
+    model_name = issue_title.replace("[MODEL ADD REQUEST] ", "").replace("-", "_")
 
-with open(f"{models_dir}/{model_name}.json", "w") as f:
-    json.dump(issue_body, f, indent=4)
+    models_dir = "./data/models"
+
+    with open(f"{models_dir}/{model_name}.json", "w") as f:
+        json.dump(issue_body, f, indent=4)
+
+
+if args.issue_number is None:
+    issues_list = fetch_all_issues()
+    if issues_list is None:
+        print("Failed to fetch issues")
+        exit(1)
+    for issue in issues_list:
+        if issue["title"].startswith("[MODEL ADD REQUEST]"):
+            convert_issue_to_model(issue)
+else:
+    issue = fetch_issue(args.issue_number)
+    if issue is None:
+        print(f"Failed to fetch issue {args.issue_number}")
+        exit(1)
+    convert_issue_to_model(issue)
