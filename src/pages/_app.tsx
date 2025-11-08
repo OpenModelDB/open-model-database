52321bc1971b065a79a532e914b2711325a6fc21
Joey Ballentine <joeyjballentine@gmail.com>
2025-11-08 14:39:41 -0600

    Update font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,5 @@
+import { Inter } from 'next/font/google';
+import { useEffect } from 'react';
 import '../styles/globals.scss';
 import 'react-tooltip/dist/react-tooltip.css';
 import { DevicePixelRatioProvider } from '../lib/hooks/use-device-pixel-ratio';
@@ -5,14 +7,27 @@ import { TooltipProvider } from '../lib/hooks/use-tooltip';
 import { WebApiProvider } from '../lib/hooks/use-web-api';
 import type { AppProps } from 'next/app';
 
+const inter = Inter({
+    subsets: ['latin'],
+    variable: '--font-inter',
+    display: 'swap',
+});
+
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        // Ensure the font variable is available on the root element
+        document.documentElement.style.setProperty('--font-inter', inter.style.fontFamily);
+    }, []);
+
     return (
-        <WebApiProvider>
-            <DevicePixelRatioProvider>
-                <TooltipProvider>
-                    <Component {...pageProps} />
-                </TooltipProvider>
-            </DevicePixelRatioProvider>
-        </WebApiProvider>
+        <div className={inter.variable}>
+            <WebApiProvider>
+                <DevicePixelRatioProvider>
+                    <TooltipProvider>
+                        <Component {...pageProps} />
+                    </TooltipProvider>
+                </DevicePixelRatioProvider>
+            </WebApiProvider>
+        </div>
     );
 }
